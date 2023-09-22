e3c735b36a57f6913ef373577b45a3dd9722b479
Juan F Gonzalez <seminariopv@hotmail.com>
2023-09-22 17:58:59 -0500

    Remove Transcriber

--- process_episode.py
+++ process_episode.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from whisper import Transcriber
+import whisper 
 
 st.title("Podcast Transcriber")
 
@@ -10,21 +10,21 @@ audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
 process_button = st.sidebar.button("Transcribe Episode")
 st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
 
-# model = whisper.load_model("base")
+model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
 
 if process_button:
     if audio_file is not None:
         st.sidebar.info("Transcribing audio...")
         audio_data = audio_file.read()
-        audio = whisper.load_audio("audio.mp3")
+        st.markdown(audio_data)
+        audio = whisper.load_audio(audio_file)
         audio = whisper.pad_or_trim(audio)
-        transcriber = Transcriber()
+        
         # result = model.transcribe("NW-S4-Demo-Video.mp3")
-        result = transcriber.transcribe(audio)
+        result = model.transcribe(audio)
         
         st.sidebar.success("Transcription Complete")
-        
         st.markdown(result['text'])
     else:
         st.sidebar.error("Please upload an episode.")
