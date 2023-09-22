14c4b4dd78d2b5018d9504bb55678eec5d6219b4
Juan F Gonzalez <seminariopv@hotmail.com>
2023-09-22 17:53:47 -0500

    Use Transcriber

--- process_episode.py
+++ process_episode.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import whisper
+from openai_whisper import Transcriber
 
 st.title("Podcast Transcriber")
 
@@ -10,17 +10,18 @@ audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
 process_button = st.sidebar.button("Transcribe Episode")
 st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
 
-model = whisper.load_model("base")
+# model = whisper.load_model("base")
 st.text("Whisper Model Loaded")
 
 if process_button:
     if audio_file is not None:
         st.sidebar.info("Transcribing audio...")
-        st.text(audio_file.read())
+        audio_data = audio_file.read()
         audio = whisper.load_audio("audio.mp3")
         audio = whisper.pad_or_trim(audio)
+        transcriber = Transcriber()
         # result = model.transcribe("NW-S4-Demo-Video.mp3")
-        result = model.transcribe(audio)
+        result = transcriber.transcribe(audio)
         
         st.sidebar.success("Transcription Complete")
         
