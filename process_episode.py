06f1550dfd2515ebb30b81c656ab85ca55b894e8
Juan F Gonzalez <seminariopv@hotmail.com>
2023-09-22 15:13:50 -0500

    Simplify code

--- process_episode.py
+++ process_episode.py
@@ -1,37 +1,24 @@
 import streamlit as st
 import whisper
 
-def main():
-    st.title("Podcast Transcriber")
-    
-    st.sidebar.header("Transcription section")
-    
-    audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
-    
-    process_button = st.sidebar.button("Transcribe Episode")
-    st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
-    
-    if process_button:
-        if audio_file is not None:
-            st.text("Loading Whisper Model...")
-            episode_info = transcribe_episode(audio_file)
-            
-            st.markdown(episode_info['text'])
-        else:
-            st.sidebar.error("Please upload an episode.")
-            
-def transcribe_episode(audio_file):
-    st.text(audio_file)
-    model = whisper.load_model("base")
-    st.text("Whisper Model Loaded")
+st.title("Podcast Transcriber")
 
-    audio = whisper.load_audio(audio_file.name)
-    st.sidebar.info("Transcribing audio...")
-    
-    result = model.transcribe(audio)
-    st.sidebar.success("Transcription Complete")
+st.sidebar.header("Transcription section")
 
-    return result
-    
-if __name__ == '__main__':
-    main()
+audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
+
+process_button = st.sidebar.button("Transcribe Episode")
+st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
+
+model = whisper.load_model("base")
+st.text("Whisper Model Loaded")
+
+if process_button:
+    if audio_file is not None:
+        st.sidebar.info("Transcribing audio...")
+        result = model.transcribe(audio_file.name)
+        st.sidebar.success("Transcription Complete")
+        
+        st.markdown(episode_info['text'])
+    else:
+        st.sidebar.error("Please upload an episode.")
