0772234dc047de346daac75836c22a1dd1e3ae74
Juan F Gonzalez <seminariopv@hotmail.com>
2023-09-22 15:59:24 -0500

    Change to cuda

--- process_episode.py
+++ process_episode.py
@@ -10,13 +10,13 @@ audio_file = st.file_uploader("Upload audio", type=["wav", "mp3", "m4a"])
 process_button = st.sidebar.button("Transcribe Episode")
 st.sidebar.markdown("**Note**: Processing can take up to 5 mins, please be patient.")
 
-model = whisper.load_model("base")
+model = whisper.load_model("base", device="cuda")
 st.text("Whisper Model Loaded")
 
 if process_button:
     if audio_file is not None:
         st.sidebar.info("Transcribing audio...")
-        result = model.transcribe(audio_file)
+        result = model.transcribe(audio_file.name)
         st.sidebar.success("Transcription Complete")
         
         st.markdown(episode_info['text'])
